seancpeters/templating

Help & usage refactor mostly complete.

src/Microsoft.TemplateEngine.Cli/TemplateListResolutionResult.cs
@@ -10,39 +10,50 @@ namespace Microsoft.TemplateEngine.Cli
 {
     public class TemplateListResolutionResult
     {
-        public TemplateListResolutionResult(bool hasUserInputLanguage)
+        public TemplateListResolutionResult(string templateName, string userInputLanguage, IReadOnlyCollection<IFilteredTemplateInfo> coreMatchedTemplates, IReadOnlyCollection<IFilteredTemplateInfo> allTemplatesInContext)
         {
-            _hasUserInputLanguage = hasUserInputLanguage;
+            _templateName = templateName;
+            _hasUserInputLanguage = !string.IsNullOrEmpty(userInputLanguage);
+            _coreMatchedTemplates = coreMatchedTemplates;
+            _allTemplatesInContext = allTemplatesInContext;
+            _bestTemplateMatchList = null;
+            _usingContextMatches = false;
         }
 
+        private readonly string _templateName;
         private readonly bool _hasUserInputLanguage;
 
-        public IReadOnlyList<IFilteredTemplateInfo> CoreMatchedTemplates { get; set; }
+        private readonly IReadOnlyCollection<IFilteredTemplateInfo> _coreMatchedTemplates;
+        private readonly IReadOnlyCollection<IFilteredTemplateInfo> _allTemplatesInContext;
 
-        public bool HasCoreMatchedTemplatesWithDisposition(Func<IFilteredTemplateInfo, bool> filter)
+        private IReadOnlyList<IFilteredTemplateInfo> _bestTemplateMatchList;
+        private bool _usingContextMatches;
+
+        public bool TryGetCoreMatchedTemplatesWithDisposition(Func<IFilteredTemplateInfo, bool> filter, out IReadOnlyList<IFilteredTemplateInfo> matchingTemplates)
         {
-            return CoreMatchedTemplates != null
-                    && CoreMatchedTemplates.Any(filter);
+            matchingTemplates = _coreMatchedTemplates.Where(filter).ToList();
+            return matchingTemplates.Count != 0;
         }
 
         public bool TryGetUnambiguousTemplateGroupToUse(out IReadOnlyList<IFilteredTemplateInfo> unambiguousTemplateGroup)
         {
-            if (CoreMatchedTemplates.Count == 0)
+            if (_coreMatchedTemplates.Count == 0)
             {
                 unambiguousTemplateGroup = null;
                 return false;
             }
 
-            if (CoreMatchedTemplates.Count == 1)
+            if (_coreMatchedTemplates.Count == 1)
             {
-                unambiguousTemplateGroup = new List<IFilteredTemplateInfo>(CoreMatchedTemplates);
+                unambiguousTemplateGroup = new List<IFilteredTemplateInfo>(_coreMatchedTemplates);
                 return true;
             }
 
+            // maybe: only use default language if we're trying to invoke
             if (!_hasUserInputLanguage)
             {
                 // only consider default language match dispositions if the user did not specify a language.
-                List<IFilteredTemplateInfo> defaultLanguageMatchedTemplates = CoreMatchedTemplates.Where(x => x.DispositionOfDefaults
+                List<IFilteredTemplateInfo> defaultLanguageMatchedTemplates = _coreMatchedTemplates.Where(x => x.DispositionOfDefaults
                                                                             .Any(y => y.Location == MatchLocation.DefaultLanguage && y.Kind == MatchKind.Exact))
                                                                             .ToList();
 
@@ -61,16 +72,16 @@ namespace Microsoft.TemplateEngine.Cli
                 }
             }
 
-            List<IFilteredTemplateInfo> paramFiltered = CoreMatchedTemplates.Where(x => !x.HasParameterMismatch).ToList();
+            List<IFilteredTemplateInfo> paramFiltered = _coreMatchedTemplates.Where(x => !x.HasParameterMismatch).ToList();
             if (TemplateListResolver.AreAllTemplatesSameGroupIdentity(paramFiltered))
             {
                 unambiguousTemplateGroup = paramFiltered;
                 return true;
             }
 
-            if (TemplateListResolver.AreAllTemplatesSameGroupIdentity(CoreMatchedTemplates))
+            if (TemplateListResolver.AreAllTemplatesSameGroupIdentity(_coreMatchedTemplates))
             {
-                unambiguousTemplateGroup = new List<IFilteredTemplateInfo>(CoreMatchedTemplates);
+                unambiguousTemplateGroup = new List<IFilteredTemplateInfo>(_coreMatchedTemplates);
                 return true;
             }
 
@@ -80,7 +91,7 @@ namespace Microsoft.TemplateEngine.Cli
 
         public bool TryGetAllInvokableTemplates(out IReadOnlyList<IFilteredTemplateInfo> invokableTemplates)
         {
-            IEnumerable<IFilteredTemplateInfo> invokableMatches = CoreMatchedTemplates.Where(x => x.IsInvokableMatch);
+            IEnumerable<IFilteredTemplateInfo> invokableMatches = _coreMatchedTemplates.Where(x => x.IsInvokableMatch);
 
             if (invokableMatches.Any())
             {
@@ -94,7 +105,7 @@ namespace Microsoft.TemplateEngine.Cli
 
         public bool TryGetSingularInvokableMatch(out IFilteredTemplateInfo template)
         {
-            IReadOnlyList<IFilteredTemplateInfo> invokableMatches = CoreMatchedTemplates.Where(x => x.IsInvokableMatch).ToList();
+            IReadOnlyList<IFilteredTemplateInfo> invokableMatches = _coreMatchedTemplates.Where(x => x.IsInvokableMatch).ToList();
             if (invokableMatches.Count() == 1)
             {
                 template = invokableMatches.First();
@@ -110,6 +121,65 @@ namespace Microsoft.TemplateEngine.Cli
 
             template = null;
             return false;
+        }
+
+        public IReadOnlyList<IFilteredTemplateInfo> BestTemplateMatchList
+        {
+            get
+            {
+                if (_bestTemplateMatchList == null)
+                {
+                    IReadOnlyList<IFilteredTemplateInfo> templateList;
+
+                    Console.Write("*** GetBestTemplateMatchList()... ");
+                    if (TryGetUnambiguousTemplateGroupToUse(out templateList))
+                    {
+                        Console.WriteLine("Unambiguous");
+                        _bestTemplateMatchList = templateList;
+                    }
+                    else if (!string.IsNullOrEmpty(_templateName) && TryGetAllInvokableTemplates(out templateList))
+                    {
+                        Console.WriteLine("All Invokable");
+                        _bestTemplateMatchList = templateList;
+                    }
+                    else if (TryGetCoreMatchedTemplatesWithDisposition(x => x.IsMatch, out templateList))
+                    {
+                        Console.WriteLine("IsMatch");
+                        _bestTemplateMatchList = templateList;
+                    }
+                    else if (TryGetCoreMatchedTemplatesWithDisposition(x => x.IsMatchExceptContext, out templateList))
+                    {
+                        Console.WriteLine("IsMatchExceptContext");
+                        _bestTemplateMatchList = templateList;
+                    }
+                    else if (TryGetCoreMatchedTemplatesWithDisposition(x => x.IsPartialMatch, out templateList))
+                    {
+                        Console.WriteLine("IsPartialMatch");
+                        _bestTemplateMatchList = templateList;
+                    }
+                    else if (TryGetCoreMatchedTemplatesWithDisposition(x => x.IsPartialMatchExceptContext, out templateList))
+                    {
+                        Console.WriteLine("IsPartialMatchExceptContext");
+                        _bestTemplateMatchList = templateList;
+                    }
+                    else
+                    {
+                        Console.WriteLine("all in context");
+                        _bestTemplateMatchList = _allTemplatesInContext.ToList();
+                        _usingContextMatches = true;
+                    }
+                }
+
+                return _bestTemplateMatchList;
+            }
+        }
+
+        public bool UsingContextMatches
+        {
+            get
+            {
+                return _usingContextMatches;
+            }
         }
     }
 }

GangWang01/pandaria

TemplateListResolutionResult.cs
@@ -10,39 +10,50 @@ namespace Microsoft.TemplateEngine.Cli
 {
     public class TemplateListResolutionResult
     {
-        public TemplateListResolutionResult(bool hasUserInputLanguage)
+        public TemplateListResolutionResult(string templateName, string userInputLanguage, IReadOnlyCollection<ITemplateMatchInfo> coreMatchedTemplates, IReadOnlyCollection<ITemplateMatchInfo> allTemplatesInContext)
         {
-            _hasUserInputLanguage = hasUserInputLanguage;
+            _templateName = templateName;
+            _hasUserInputLanguage = !string.IsNullOrEmpty(userInputLanguage);
+            _coreMatchedTemplates = coreMatchedTemplates;
+            _allTemplatesInContext = allTemplatesInContext;
+            _bestTemplateMatchList = null;
+            _usingContextMatches = false;
         }
 
+        private readonly string _templateName;
         private readonly bool _hasUserInputLanguage;
 
-        public IReadOnlyList<ITemplateMatchInfo> CoreMatchedTemplates { get; set; }
+        private readonly IReadOnlyCollection<ITemplateMatchInfo> _coreMatchedTemplates;
+        private readonly IReadOnlyCollection<ITemplateMatchInfo> _allTemplatesInContext;
 
-        public bool HasCoreMatchedTemplatesWithDisposition(Func<ITemplateMatchInfo, bool> filter)
+        private IReadOnlyList<ITemplateMatchInfo> _bestTemplateMatchList;
+        private bool _usingContextMatches;
+
+        public bool TryGetCoreMatchedTemplatesWithDisposition(Func<ITemplateMatchInfo, bool> filter, out IReadOnlyList<ITemplateMatchInfo> matchingTemplates)
         {
-            return CoreMatchedTemplates != null
-                    && CoreMatchedTemplates.Any(filter);
+            matchingTemplates = _coreMatchedTemplates.Where(filter).ToList();
+            return matchingTemplates.Count != 0;
         }
 
         public bool TryGetUnambiguousTemplateGroupToUse(out IReadOnlyList<ITemplateMatchInfo> unambiguousTemplateGroup)
         {
-            if (CoreMatchedTemplates.Count == 0)
+            if (_coreMatchedTemplates.Count == 0)
             {
                 unambiguousTemplateGroup = null;
                 return false;
             }
 
-            if (CoreMatchedTemplates.Count == 1)
+            if (_coreMatchedTemplates.Count == 1)
             {
-                unambiguousTemplateGroup = new List<ITemplateMatchInfo>(CoreMatchedTemplates);
+                unambiguousTemplateGroup = new List<ITemplateMatchInfo>(_coreMatchedTemplates);
                 return true;
             }
 
+            // maybe: only use default language if we're trying to invoke
             if (!_hasUserInputLanguage)
             {
                 // only consider default language match dispositions if the user did not specify a language.
-                List<ITemplateMatchInfo> defaultLanguageMatchedTemplates = CoreMatchedTemplates.Where(x => x.DispositionOfDefaults
+                List<ITemplateMatchInfo> defaultLanguageMatchedTemplates = _coreMatchedTemplates.Where(x => x.DispositionOfDefaults
                                                                             .Any(y => y.Location == MatchLocation.DefaultLanguage && y.Kind == MatchKind.Exact))
                                                                             .ToList();
 
@@ -61,17 +72,16 @@ namespace Microsoft.TemplateEngine.Cli
                 }
             }
 
-            List<ITemplateMatchInfo> paramFiltered = CoreMatchedTemplates.Where(x => !x.HasParameterMismatch()).ToList();
-
+            List<ITemplateMatchInfo> paramFiltered = _coreMatchedTemplates.Where(x => !x.HasParameterMismatch()).ToList();
             if (TemplateListResolver.AreAllTemplatesSameGroupIdentity(paramFiltered))
             {
                 unambiguousTemplateGroup = paramFiltered;
                 return true;
             }
 
-            if (TemplateListResolver.AreAllTemplatesSameGroupIdentity(CoreMatchedTemplates))
+            if (TemplateListResolver.AreAllTemplatesSameGroupIdentity(_coreMatchedTemplates))
             {
-                unambiguousTemplateGroup = new List<ITemplateMatchInfo>(CoreMatchedTemplates);
+                unambiguousTemplateGroup = new List<ITemplateMatchInfo>(_coreMatchedTemplates);
                 return true;
             }
 
@@ -81,7 +91,7 @@ namespace Microsoft.TemplateEngine.Cli
 
         public bool TryGetAllInvokableTemplates(out IReadOnlyList<ITemplateMatchInfo> invokableTemplates)
         {
-            IEnumerable<ITemplateMatchInfo> invokableMatches = CoreMatchedTemplates.Where(x => x.IsInvokableMatch());
+            IEnumerable<ITemplateMatchInfo> invokableMatches = _coreMatchedTemplates.Where(x => x.IsInvokableMatch());
 
             if (invokableMatches.Any())
             {
@@ -95,8 +105,8 @@ namespace Microsoft.TemplateEngine.Cli
 
         public bool TryGetSingularInvokableMatch(out ITemplateMatchInfo template)
         {
-            IReadOnlyList<ITemplateMatchInfo> invokableMatches = CoreMatchedTemplates.Where(x => x.IsInvokableMatch()).ToList();
-            if (invokableMatches.Count == 1)
+            IReadOnlyList<ITemplateMatchInfo> invokableMatches = _coreMatchedTemplates.Where(x => x.IsInvokableMatch()).ToList();
+            if (invokableMatches.Count() == 1)
             {
                 template = invokableMatches[0];
                 return true;
@@ -111,6 +121,65 @@ namespace Microsoft.TemplateEngine.Cli
 
             template = null;
             return false;
+        }
+
+        public IReadOnlyList<ITemplateMatchInfo> BestTemplateMatchList
+        {
+            get
+            {
+                if (_bestTemplateMatchList == null)
+                {
+                    IReadOnlyList<ITemplateMatchInfo> templateList;
+
+                    Console.Write("*** GetBestTemplateMatchList()... ");
+                    if (TryGetUnambiguousTemplateGroupToUse(out templateList))
+                    {
+                        Console.WriteLine("Unambiguous");
+                        _bestTemplateMatchList = templateList;
+                    }
+                    else if (!string.IsNullOrEmpty(_templateName) && TryGetAllInvokableTemplates(out templateList))
+                    {
+                        Console.WriteLine("All Invokable");
+                        _bestTemplateMatchList = templateList;
+                    }
+                    else if (TryGetCoreMatchedTemplatesWithDisposition(x => x.IsMatch, out templateList))
+                    {
+                        Console.WriteLine("IsMatch");
+                        _bestTemplateMatchList = templateList;
+                    }
+                    else if (TryGetCoreMatchedTemplatesWithDisposition(x => x.IsMatchExceptContext(), out templateList))
+                    {
+                        Console.WriteLine("IsMatchExceptContext");
+                        _bestTemplateMatchList = templateList;
+                    }
+                    else if (TryGetCoreMatchedTemplatesWithDisposition(x => x.IsPartialMatch, out templateList))
+                    {
+                        Console.WriteLine("IsPartialMatch");
+                        _bestTemplateMatchList = templateList;
+                    }
+                    else if (TryGetCoreMatchedTemplatesWithDisposition(x => x.IsPartialMatchExceptContext(), out templateList))
+                    {
+                        Console.WriteLine("IsPartialMatchExceptContext");
+                        _bestTemplateMatchList = templateList;
+                    }
+                    else
+                    {
+                        Console.WriteLine("all in context");
+                        _bestTemplateMatchList = _allTemplatesInContext.ToList();
+                        _usingContextMatches = true;
+                    }
+                }
+
+                return _bestTemplateMatchList;
+            }
+        }
+
+        public bool UsingContextMatches
+        {
+            get
+            {
+                return _usingContextMatches;
+            }
         }
     }
 }